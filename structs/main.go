9c7e748c6d9b8151f9b71528539f049f73eae151
Keith Price <keithprice.dev@gmail.com>
2024-11-04 08:04:40 +0700

    work on structs project

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,33 @@
 package main
 
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email    string
+	postCode int
+}
+
 func main() {
 
+	MrMucus := person{
+		firstName: "Yeiling",
+		lastName:  "Loops",
+		contactInfo: contactInfo{
+			email:    "yelpers@screamers.com",
+			postCode: 45265,
+		},
+	}
+
+	MrMucus.print()
+
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
