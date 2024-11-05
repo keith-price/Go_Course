a3263a52965df59b9d18b00b86b39e35b1b9ab97
Keith Price <keithprice.dev@gmail.com>
2024-11-05 11:10:32 +0700

    added new interfaces challenge

---
+++ interface_challenge/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type triangle struct {
+	height float64
+	base   float64
+}
+type square struct {
+	sideLength float64
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func main() {
+	t := triangle{
+		height: 64,
+		base:   67,
+	}
+
+	s := square{
+		sideLength: 67,
+	}
+
+	printArea(t)
+	printArea(s)
+}
+
+func printArea(sh shape) {
+	fmt.Println(sh.getArea())
+}
+
+func (t triangle) getArea() float64 {
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	return s.sideLength * s.sideLength
+}
--- interfaces_http/main.go
+++ interfaces_http/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
 
+func main() {
+	res, err := http.Get("http://google.com")
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+
+	// bs := make([]byte, 99999)
+
+	// res.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	io.Copy(os.Stdout, res.Body)
 }
